bc345127fef0622b68aa63c943a404f36a683fec
Alan Alvarado <alan.g2.0@hotmail.com>
2026-09-06 15:02:26 -0300

    Adicionando um main real

--- Servidor/main.py
+++ Servidor/main.py
@@ -1,1 +1,7 @@
-print("oi")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"status": "Servidor rodando no celular!", "dispositivo": "ESP8266 Gerenciador"}
